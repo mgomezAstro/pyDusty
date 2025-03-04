dd388a684970d2666642c624cc884ba6724c604f
mgomeAstro <29342489+mgomezAstro@users.noreply.github.com>
2025-03-04 17:23:31 +0100

    Fixed the toml file.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from pydusty.__version__ import __version__
 
 setup(
     name="pydusty",
     version=__version__,
-    packages=["pydusty"],
+    packages=find_packages(),
+    install_requires=["miepython"],
     description="Python wrapper for the radiative transfer code Dusty (v4) originally created by Maia Nenkova (2000ASPC..196...77N).",
     author="M. A. Gomez-Munoz",
     author_email="mgomez_astro@outlook.com",
