8af695718b7e2509ef31a360b6a6bb844e78ebdb
mgomeAstro <29342489+mgomezAstro@users.noreply.github.com>
2025-08-26 16:28:31 +0200

    Changed minor version from 0.3.0 to 0.4.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pydusty",
-    version="0.3.0",
+    version="0.4.0",
     packages=find_packages(),
     package_data={"pydusty": ["data/*.txt"]},
     install_requires=["miepython >= 3.0"],
