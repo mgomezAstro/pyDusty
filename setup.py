6e016fa7109adf3319b3bb3b8ccd8a35f6392a08
mgomeAstro <29342489+mgomezAstro@users.noreply.github.com>
2025-08-28 15:20:20 +0200

    Fixed sutom opacities.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pydusty",
-    version="0.5.1",
+    version="0.5.2",
     packages=find_packages(),
     package_data={"pydusty": ["data/*.txt"]},
     install_requires=["miepython >= 3.0"],
